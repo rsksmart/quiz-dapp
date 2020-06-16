bde096f2e2e8875c5f93b03f0fbbe1510649631c
Dulce Villarreal <dulcedu@gmail.com>
2020-06-16 01:35:10 -0500

    trying to fix the strings

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -1,39 +1,39 @@
+// pragma solidity >=0.4.21 <0.7.0;
 pragma solidity ^0.4.14;
 import "./strings.sol";
-
+import "./stringUtils.sol";
 contract Quiz {
     using strings for *;
-
-    string[] rightAnswers = ["c", "a", "a", "b"];
+    address public owner;
+    string[4] rightAnswers = ["c", "a", "a", "b"];
     address[] public players;
     uint8[] public playerHits;
-
+    string public questions;
     constructor() public {
+        owner = msg.sender;
     }
-
     function getPlayerHits() public view returns (uint8[] memory) {
         return playerHits;
     }
-
     function getPlayers() public view returns (address[] memory) {
         return players;
     }
-
-    function answerQuestions (string memory playerAnswers) public returns (string memory) {
-        uint hits = 0;
+    function answerQuestions (string playerAnswers) public returns (string memory) {
+        uint hits;
         var s = playerAnswers.toSlice();
         var delim = ".".toSlice();
         var parts = new string[](s.count(delim) + 1);
-
         for (uint i = 0; i < parts.length; i++) {
             parts[i] = s.split(delim).toString();
-            if (parts[i] == rightAnswers[i]) {
+            // if (parts[i] == rightAnswers[i]) {
+            if (StringUtils.equal(parts[i], rightAnswers[i])) {
                 hits++;
             }
         }
         players.push(msg.sender);
-        hits.push(hits);
-
+        playerHits.push(4);
+        // players[players.length] = msg.sender;
+        // hits[index] = 0;
         return playerAnswers;
     }
 }

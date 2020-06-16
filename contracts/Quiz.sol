c4188d4967b6f75b3e35add006dd25478a65d96a
Dulce Villarreal <dulcedu@gmail.com>
2020-06-15 23:27:48 -0500

    Add the string support for solidity

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.14;
+import "./strings.sol";
 
 contract Quiz {
+    using strings for *;
+
     string[] rightAnswers = ["c", "a", "a", "b"];
     address[] public players;
     uint8[] public playerHits;
@@ -17,5 +20,20 @@ contract Quiz {
     }
 
     function answerQuestions (string playerAnswers) public returns (string memory) {
+    uint hits = 0;
+    var s = playerAnswers.toSlice();
+    var delim = ".".toSlice();
+    var parts = new string[](s.count(delim) + 1);
+
+    for (uint i = 0; i < parts.length; i++) {
+        parts[i] = s.split(delim).toString();
+        if (parts[i] == rightAnswers[i]) {
+            hits++;
+        }
+    }
+    players.push(msg.sender);
+    hits.push(hits);
+
+    return playerAnswers;
     }
 }

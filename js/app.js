24bba4aa9383f620193f1a6e13cfe03787cb2562
Dulce Villarreal <dulcedu@gmail.com>
2020-06-15 23:19:16 -0500

    Add sendQuestions method

--- js/app.js
+++ js/app.js
@@ -7,12 +7,47 @@ var app = new Vue({
       questions: []
     },
     mounted () {
-      axios.get('http://localhost:8500/bzz-raw:/6ab1d7b2a738796daaeafb4c44afcb643b2f8d3d7e7af706b9e1115c0a69c6c8')
+      this.initWeb3()
+        .then(() => {
+          console.log('App initialized')
+        })
+    },
+      /*axios.get('http://localhost:8500/bzz-raw:/6ab1d7b2a738796daaeafb4c44afcb643b2f8d3d7e7af706b9e1115c0a69c6c8')
         .then(response => {
           this.questions = response.data.questions
         })
-    },
+    },*/
     methods: {
+      async initWeb3() {
+        await axios.get('http://localhost:8500/bzz-raw:/07f1112168025f4c309b652fb364b9ea126728e7b0959338b0bf9f5187d474d0')
+          .then(response => {
+            this.questions = response.data.questions
+          })
+          if (window.ethereum) {
+            this.web3Provider = window.ethereum
+            try {
+              // request account access
+              await window.ethereum.enable()
+            } catch (error) {
+              console.error("user denied account access")
+            }
+          } else if (window.web3) {
+            this.web3Provider = window.web3.currentProvider
+          } else {
+            this.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545')
+          }
+          web3 = new Web3(this.web3Provider)
+          return this.initContract()
+      },
+      initContract: function (params) {
+        return axios.get('../build/contracts/Quiz.json')
+          .then(response => {
+            const QuizArtifact = response.data
+            this.contracts.Quiz = TruffleContract(QuizArtifact)
+            // Set the provider for our contract
+            this.contracts.Quiz.setProvider(this.web3Provider)
+          })
+      },
       closeModal: function () {
         document.querySelector('div.modal').classList.remove('is-active')
       },
@@ -39,6 +74,33 @@ var app = new Vue({
             })
           }, 1000)
         }
+      },
+      sendQuestions: function () {
+        const contract = this.contracts.Quiz
+        web3.eth.getAccounts((error, accounts) => {
+          if (error) {
+            console.error(error)
+          }
+
+          const account = accounts[0]
+
+          this.contracts.Quiz.deployed()
+              .then(instance => {
+                  const quizInstance = instance
+
+                  return quizInstance.answerQuestions(this.answers.join("."), { from: account })
+              })
+              .then(result => {
+                  document.querySelector('div.modal').classList.remove('is-active')
+              })
+              .catch(err => {
+                  console.error("=======================")
+                  console.error(err)
+                  console.error(err.message)
+                  console.error(err.stack)
+                  console.error('=======================')
+              })
+        })
       },
       getQuestion: function (index) {
         for (let i = 0; i < this.questions.length; i++) {

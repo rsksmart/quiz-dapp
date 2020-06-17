27c26382b45891fa1dbb0b251a205baaaf13edf7
Dulce Villarreal <dulcedu@gmail.com>
2020-06-16 21:42:00 -0500

    Delete the import for strings libraries, quiz.sol Change strings to numeric

--- contracts/Quiz.sol
+++ contracts/Quiz.sol
@@ -1,39 +1,34 @@
-// pragma solidity >=0.4.21 <0.7.0;
 pragma solidity ^0.4.14;
-import "./strings.sol";
-import "./stringUtils.sol";
 contract Quiz {
-    using strings for *;
     address public owner;
-    string[4] rightAnswers = ["c", "a", "a", "b"];
+    uint8[4] rightAnswers = [3, 1, 1, 2];
     address[] public players;
     uint8[] public playerHits;
-    string public questions;
+
+    event AnswerEvent(uint8 hitsCounter);
+
     constructor() public {
         owner = msg.sender;
     }
+
     function getPlayerHits() public view returns (uint8[] memory) {
         return playerHits;
     }
+
     function getPlayers() public view returns (address[] memory) {
         return players;
     }
-    function answerQuestions (string playerAnswers) public returns (string memory) {
-        uint hits;
-        var s = playerAnswers.toSlice();
-        var delim = ".".toSlice();
-        var parts = new string[](s.count(delim) + 1);
-        for (uint i = 0; i < parts.length; i++) {
-            parts[i] = s.split(delim).toString();
-            // if (parts[i] == rightAnswers[i]) {
-            if (StringUtils.equal(parts[i], rightAnswers[i])) {
+
+    function answerQuestions (uint8[] playerAnswers) public returns (uint8) {
+        uint8 hits;
+        for (uint i = 0; i < playerAnswers.length; i++) {
+            if (playerAnswers[i] == rightAnswers[i]) {
                 hits++;
             }
         }
         players.push(msg.sender);
-        playerHits.push(4);
-        // players[players.length] = msg.sender;
-        // hits[index] = 0;
-        return playerAnswers;
+        playerHits.push(hits);
+        emit AnswerEvent(hits);
+        return hits;
     }
 }
